f2049aff63d8dc5d5df21fe93baf460431c7a972
Mat Cross <mathewc@nag.co.uk>
2021-04-24 09:39:45 +0100

    Ran through pylint.

--- scripts/implied_volatility.py
+++ scripts/implied_volatility.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 Example for Implied Volatility using the NAG Library for Python
@@ -10,8 +10,10 @@ Make sure to download data during CBOE Trading Hours.
 Updated for NAG Library for Python Mark 27.1
 """
 
+# pylint: disable=invalid-name,too-many-branches,too-many-locals,too-many-statements
+
 try:
-    import os,sys
+    import sys
     import pandas
     import numpy as np
     import matplotlib.pylab as plt
@@ -19,10 +21,12 @@ try:
     from naginterfaces.library import specfun, fit
     from naginterfaces.base import utils
     from matplotlib import cm
-    from mpl_toolkits.mplot3d import axes3d
 
 except ImportError as e:
-    print("Could not import the following module. Do you have a working installation of the NAG Library for Python?")
+    print(
+        "Could not import the following module. "
+        "Do you have a working installation of the NAG Library for Python?"
+    )
     print(e)
     sys.exit(1)
 
@@ -38,11 +42,10 @@ cumulative_month = {'Jan': 31, 'Feb': 57, 'Mar': 90,
                     'Jul': 212, 'Aug': 243, 'Sep': 273,
                     'Oct': 304, 'Nov': 334, 'Dec': 365}
 
-
-def main():
+def main(): # pylint: disable=missing-function-docstring
 
     try:
-        if(len(sys.argv)>1):
+        if len(sys.argv)>1:
             QuoteData = sys.argv[1]
         else:
             QuoteData = 'QuoteData.dat'
@@ -52,9 +55,9 @@ def main():
         qd_head.append(qd.readline())
         qd_head.append(qd.readline())
         qd.close()
-    except:
+    except: # pylint: disable=bare-except
         sys.stderr.write("Usage: implied_volatility.py QuoteData.dat\n")
-        sys.stderr.write("Couldn't read QuoteData")
+        sys.stderr.write("Couldn't read QuoteData\n")
         sys.exit(1)
 
     print("Implied Volatility for %s %s" % (qd_head[0].strip(), qd_head[1]))
@@ -63,7 +66,7 @@ def main():
     first = qd_head[0].split(',')
     second = qd_head[1].split()
     qd_date = qd_head[1].split(',')[0]
-    
+
     company = first[0]
     underlyingprice = float(first[1])
     month, day = second[:2]
@@ -86,7 +89,7 @@ def main():
     # Need to fill the NA values in dataframe
     data = data.fillna(0.0)
 
-    # Let's look at data where there was a recent sale 
+    # Let's look at data where there was a recent sale
     data = data[(data['Last Sale'] > 0) | (data['Last Sale.1'] > 0)]
 
     # Get the Options Expiration Date
@@ -98,26 +101,26 @@ def main():
     strike.name = 'Strike'
 
     data = data.join(exp).join(strike)
-    
+
     print("Number of data points found: {}\n".format(len(data.index)))
-        
+
     print('Calculating Implied Vol of Calls...')
     r = np.zeros(len(data.index))
     t = (data.Expiration - today)/365.0
     s0 = np.full(len(data.index),underlyingprice)
     pCall= (data.Bid + data.Ask) / 2
-    
+
     # A lot of the data is incomplete or extreme so we tell the NAG routine
     # not to worry about warning us about data points it can't work with
     warnings.simplefilter('ignore',utils.NagAlgorithmicWarning)
-    sigmaCall, ivalidCall = specfun.opt_imp_vol('C',pCall,data.Strike, s0,t,r,mode = 1)
+    sigmaCall = specfun.opt_imp_vol('C',pCall,data.Strike, s0,t,r,mode = 1).sigma
     impvolcall = pandas.Series(sigmaCall,index=data.index, name='impvolCall')
 
     data = data.join(impvolcall)
 
     print('Calculating Implied Vol of Puts...')
     pPut= (data['Bid.1'] + data['Ask.1']) / 2
-    sigmaPut, ivalidPut = specfun.opt_imp_vol('P',pPut,data.Strike, s0,t,r,mode = 1)
+    sigmaPut = specfun.opt_imp_vol('P',pPut,data.Strike, s0,t,r,mode = 1).sigma
     impvolput = pandas.Series(sigmaPut,index=data.index, name='impvolPut')
 
     data = data.join(impvolput)
@@ -128,7 +131,7 @@ def main():
     # Encode graph layout: 3 rows, 3 columns, 1 is first graph.
     num = 331
     max_xticks = 4
-    
+
     for date in dates:
         # add each subplot to the figure
         plot_year, plot_month = date.split()
@@ -154,20 +157,19 @@ def main():
 
     plt.suptitle('Implied Volatility for %s Current Price: %s Date: %s' %
                  (company, underlyingprice, qd_date))
-    
+
 
     print("\nPlotting Volatility Curves/Surface")
-    """
-    The code below will plot the Volatility Surface
-    It uses e02ca to fit with a polynomial and e02cb to evaluate at 
-    intermediate points
-    """ 
+
+    # The code below will plot the Volatility Surface
+    # It uses fit.dim2_cheb_lines to fit with a polynomial and
+    # fit.dim2_cheb_eval to evaluate at intermediate points
 
     m = np.empty(len(dates), dtype=np.int32)
     y = np.empty(len(dates), dtype=np.double)
-    xmin = np.empty(len(dates), dtype=np.double)    
+    xmin = np.empty(len(dates), dtype=np.double)
     xmax = np.empty(len(dates), dtype=np.double)
- 
+
     data = data.sort_values(by=['Strike']) # Need to sort for NAG Algorithm
 
     k = 3   # this is the degree of polynomial for x-axis (Strike Price)
@@ -178,18 +180,17 @@ def main():
     for date in dates:
         plot_year, plot_month = date.split()
         plot_date = (int(plot_year) - (current_year % 2000)) * 365 + cumulative_month[plot_month]
-        
-        call_data = data[(data.Expiration == plot_date) & 
-				(data.impvolPut > .01) & 
+
+        call_data = data[(data.Expiration == plot_date) &
+				(data.impvolPut > .01) &
 				(data.impvolPut < 1) &
                                 (data['Last Sale.1'] > 0)]
-         
-        exp_sizes = call_data.Expiration.size
-        if(exp_sizes > 0):       
-            m[i] = exp_sizes
-            n = len(dates)
 
-            if(i == 0):
+        exp_sizes = call_data.Expiration.size
+        if exp_sizes > 0:
+            m[i] = exp_sizes
+
+            if i == 0:
                 x = np.array(call_data.Strike)
                 call = np.array(call_data.impvolPut)
                 xmin[0] = x.min()
@@ -205,47 +206,45 @@ def main():
             i+=1
     nux = np.zeros(1,dtype=np.double)
     nuy = np.zeros(1,dtype=np.double)
-    inux = 1
-    inuy = 1
 
-    if(len(dates) != i):
-        print("Error with data: the CBOE may not be open for trading or one expiration date has null data")
+    if len(dates) != i:
+        print(
+            "Error with data: the CBOE may not be open for trading "
+            "or one expiration date has null data"
+        )
         return 0
     weight = np.ones(call.size, dtype=np.double)
-    
-    #Call the NAG Chebyshev fitting function
-    output_coef = fit.dim2_cheb_lines(m,k,l,x,y,call,weight,(k + 1) * (l + 1),xmin,xmax,nux,nuy)        
-   
-    """
-    Now that we have fit the function,
-    we use fit.dim2_cheb_eval to evaluate at different strikes/expirations 
-    """
 
-    nStrikes = 100 # number of Strikes to evaluate    
+    #Call the NAG Chebyshev fitting function
+    output_coef = fit.dim2_cheb_lines(m,k,l,x,y,call,weight,(k + 1) * (l + 1),xmin,xmax,nux,nuy)
+
+    # Now that we have fit the function,
+    # we use fit.dim2_cheb_eval to evaluate at different strikes/expirations
+
+    nStrikes = 100 # number of Strikes to evaluate
     spacing = 20 # number of Expirations to evaluate
-    
+
     for i in range(spacing):
-        mfirst = 1	
-        mlast = nStrikes
+        mfirst = 1
         xmin = data.Strike.min()
         xmax = data.Strike.max()
-         
+
         x = np.linspace(xmin, xmax, nStrikes)
 
         ymin = data.Expiration.min() - today
         ymax = data.Expiration.max() - today
-      
-        y = (ymin) + i * np.floor((ymax - ymin) / spacing) 
+
+        y = (ymin) + i * np.floor((ymax - ymin) / spacing)
 
         fx=np.empty(nStrikes)
 
         fx=fit.dim2_cheb_eval(mfirst,k,l,x,xmin,xmax,y,ymin,ymax,output_coef)
-        
+
         if 'xaxis' in locals():
             xaxis = np.append(xaxis, x)
             temp = np.empty(len(x))
             temp.fill(y)
-            yaxis = np.append(yaxis, temp)    
+            yaxis = np.append(yaxis, temp)
             for j in range(len(x)):
                 zaxis.append(fx[j])
         else:
@@ -255,7 +254,7 @@ def main():
             zaxis = []
             for j in range(len(x)):
                 zaxis.append(fx[j])
-   
+
     fig = plt.figure(2)
     ax = fig.add_subplot(111, projection='3d')
 
@@ -266,14 +265,14 @@ def main():
         print ("Your version of Matplotlib does not support plot_trisurf")
         print ("...plotting wireframe instead")
         ax.plot(xaxis, yaxis, zaxis)
-    
+
     ax.set_xlabel('Strike Price')
     ax.set_ylabel('Days to Expiration')
     ax.set_zlabel('Implied Volatility for Put Options')
     plt.suptitle('Implied Volatility Surface for %s Current Price: %s Date: %s' %
                  (company, underlyingprice, qd_date))
-    
-    plt.show()    
+
+    plt.show()
 
 if __name__ == "__main__":
     main()
